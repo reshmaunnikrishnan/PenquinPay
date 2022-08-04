06d4a420f3202de1f8bb65fb51258a9c85b1d119
Reshma Unnikrishnan <ruvlmoon@gmail.com>
2022-08-04 11:32:59 +0200

    Added extensions for the required parts

---
+++ PenquinPay/Extension/String+ValueType.swift
@@ -0,0 +1,19 @@
+//
+//  String+ValueType.swift
+//  PenquinPay
+//
+//  Created by Reshma Unnikrishnan on 04.08.22.
+//
+
+import Foundation
+
+extension String {
+    var hexaToInt      : Int    { return Int(strtoul(self, nil, 16))      }
+    var hexaToDouble   : Double { return Double(strtoul(self, nil, 16))   }
+    var hexaToBinary   : String { return String(hexaToInt, radix: 2)      }
+    var decimalToHexa  : String { return String(Int(self) ?? 0, radix: 16)}
+    var decimalToBinary: String { return String(Int(self) ?? 0, radix: 2) }
+    var binaryToInt    : Int    { return Int(strtoul(self, nil, 2))       }
+    var binaryToDouble : Double { return Double(strtoul(self, nil, 2))   }
+    var binaryToHexa   : String { return String(binaryToInt, radix: 16)  }
+}
